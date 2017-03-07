87fc3ed11468ee864ca6f1960c6b9b941eba9a27
newbe lee <ali@code502.com>
2017-03-07 15:12:00 +0800

    修改文件名

---
+++ play.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+
+from __future__ import print_function
+import random
+
+
+def guss_num(num):
+    while True:
+	try:
+      	    value = int(raw_input("pls input your number:"))
+ 	except ValueError , e:
+	    print(e)
+	    continue
+	if ( value > num ):
+	    print("the number is less the you input")
+	elif (value < num ):
+	    print("the number is lager the you input")
+	else:
+	    print("you are great!")
+            print("===========================")
+	    break
+
+def play_game():
+    main()
+    while True:
+        res = raw_input("Do you want to play again,pls input yes or no:")
+        if  res in ("y","yes"):
+            main()
+        else:
+	    print("========bye bye=========")
+	    break
+	    
+def main():
+    print("=======paly game=======")
+    num = random.randint(1,99)
+    guss_num(num)
+
+if __name__ == "__main__":
+     play_game()
+
