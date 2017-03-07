64889d810e00f1ef32dfd8dace68c5b3809a91ae
newbe lee <ali@code502.com>
2017-03-07 10:59:53 +0800

    增加输入类型不匹配判断以及是否继续玩游戏

--- b.py
+++ b.py
@@ -5,19 +5,35 @@ import random
 
 def guss_num(num):
     while True:
-    	a = int(raw_input("input the number:"))
-	if ( a > num ):
+	try:
+      	    value = int(raw_input("pls input your number:"))
+ 	except ValueError , e:
+	    print(e)
+	    continue
+	if ( value > num ):
 	    print("the number is less the you input")
-	elif (a < num ):
+	elif (value < num ):
 	    print("the number is lager the you input")
 	else:
 	    print("you are great!")
+            print("===========================")
 	    break
 
+def play_again():
+    main()
+    while True:
+        res = raw_input("Do you want to play again,pls input yes or no:")
+        if  res in ("y","yes"):
+            main()
+        else:
+	    print("========bye bye=========")
+	    break
+	    
 def main():
+    print("=======paly game=======")
     num = random.randint(1,99)
     guss_num(num)
 
 if __name__ == "__main__":
-    main()
+     play_again()
 
