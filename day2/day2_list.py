65232cd437499d5ed1501231b44f7884a7ed44bb
newbe lee <ali@code502.com>
2017-03-13 16:57:45 +0800

    修改字符串文档

---
+++ day2/day2_list.py
@@ -0,0 +1,7 @@
+from __future__ import  print_function
+
+# -*- coding:utf-8 -*-
+
+
+L = ["python",2,"not","like",3] 
+print L
--- day2/day2_string.py
+++ day2/day2_string.py
@@ -4,20 +4,24 @@
 并且不可以改变，可以使用单引号，双引号，三引号来定义"""
 
 #定义字符串，在ipython下可以直接使用tab键查看其方法
-In [1]: s = "python is fun"
-In [2]: s.
+s = "python is fun"
+""" s.
            s.capitalize s.decode     s.expandtabs s.index      s.isdigit    s.istitle    s.ljust      s.partition  s.rindex     s.rsplit     s.splitlines s.swapcase   s.upper      
            s.center     s.encode     s.find       s.isalnum    s.islower    s.isupper    s.lower      s.replace    s.rjust      s.rstrip     s.startswith s.title      s.zfill      
-           s.count      s.endswith   s.format     s.isalpha    s.isspace    s.join       s.lstrip     s.rfind      s.rpartition s.split      s.strip      s.translate          
-In [10]: s.split()
-Out[10]: ['python', 'is', 'fun']
+           s.count      s.endswith   s.format     s.isalpha    s.isspace    s.join       s.lstrip     s.rfind      s.rpartition s.split      s.strip      s.translate"""
+print ("按空格分隔{1},结果是{0}" .format(s.split(),s))
+"""Out: ['python', 'is', 'fun']"""
 
-In [22]: s * 2
-Out[22]: 'python is funpython is fun'
+print ("s * 2 结果是将字符串打印两遍:{0}" .format(s*2))
+"""Out: 'python is funpython is fun'"""
 
 #计算字符串长度
-In [23]: len(s)
-Out[23]: 13
+print ("length of '{1}' is {0} " .format(len(s),s))
+"""Out: 13"""
 
-In [25]: s[:6]
-Out[25]: 'python'
+#字符串截取，从开始截取到第六个
+print ("{0}从开始截取到第六个:{1}" .format(s,s[:6]))
+"""Out[25]: 'python' """
+
+print ("打印从开始到结束:{0}" .format(s[::]))
+""" Out: 'python is fun' """
